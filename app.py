28a287a594d1d4a4e356db1a880b4ba3b0d64630
pbs03531-hub <pbs03531@gmail.com>
2026-05-03 16:10:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -468,4 +468,8 @@ def logout():
     return redirect("/")
 
 if __name__ == "__main__":
-    app.run(debug=True) 
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
